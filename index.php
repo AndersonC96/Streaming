46f485c5627615514633cddbc757f2f4174e008b
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-06-27 16:52:13 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Pesquisa</title>
         <script src="https://cdn.tailwindcss.com"></script>
+        <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
     </head>
     <body class="bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white">
         <div class="min-h-screen flex items-center justify-center">
@@ -17,7 +18,7 @@
                         </svg>
                     </div>
                     <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Pesquisar" required />
-                    <button type="button" class="absolute inset-y-0 end-0 flex items-center pe-3">
+                    <button type="button" id="voice-search-button" class="absolute inset-y-0 end-0 flex items-center pe-3">
                         <svg class="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z"/>
                         </svg>
@@ -30,5 +31,25 @@
                 </button>
             </form>
         </div>
+        <script>
+            const voiceSearchButton = document.getElementById('voice-search-button');
+            const voiceSearchInput = document.getElementById('voice-search');
+            const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+            if(SpeechRecognition){
+                const recognition = new SpeechRecognition();
+                recognition.lang = 'pt-BR';
+                recognition.onresult = function(event){
+                    voiceSearchInput.value = event.results[0][0].transcript;
+                };
+                recognition.onerror = function(event){
+                    console.error('Error occurred in speech recognition: ' + event.error);
+                };
+                voiceSearchButton.addEventListener('click', function(){
+                    recognition.start();
+                });
+            }else{
+                console.log('API de reconhecimento de fala não é suportada neste navegador.');
+            }
+        </script>
     </body>
 </html>
